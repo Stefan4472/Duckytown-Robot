fe6933fc1869e3c920bd2d2348c409ff5647b9ea
Wilson <wilson.c132@gmail.com>
2019-10-18 15:27:39 -0400

    added calibration

--- quad_encoder/quad_encoder.ino
+++ quad_encoder/quad_encoder.ino
@@ -3,20 +3,21 @@
 
 QTRSensors qtr;
 
-const int8_t lookup_table[] = {0,0,0,-1,0,0,1,0,0,1,0,0,-1,0,0,0};
-const float WHEEL_BASE =  17.8; // width between wheels (in cm? floats have 6-7 digits total precision)
-const float WHEEL_RADIUS = 12; // should be same unit as WHEEL_BASE
-//const float CHASSIS_LENGTH = 26; // length from center of wheel axis to front point we are controlling 
+const long WHEEL_BASE = 12000; // width between wheels (in μm? smaller units are useful to avoid floats)
+const long WHEEL_RADIUS = 3500; // should be same unit as WHEEL_BASE
+//const long CHASSIS_LENGTH = 24000; // length from center of wheel axis to front point we are controlling 
 const uint8_t SensorCount = 4;
 uint16_t sensorValues[SensorCount];
 
-volatile long prev_left_count = 0;
+
 volatile long left_count = 0; // track # of left wheel movements, CCW is positive
-volatile long prev_right_count = 0;
 volatile long right_count = 0; // track # of right wheel movements
-volatile long x_world = 0; // x pos in world frame (in cm? should be same as WHEEL_BASE)
-volatile long y_world = 0; // y pos in world frame (in cm? should be same as WHEEL_BASE)
+volatile long x_world = 0; // x pos in world frame (in μm? should be same as WHEEL_BASE)
+volatile long y_world = 0; // y pos in world frame (in μm? should be same as WHEEL_BASE)
 volatile long theta_world = 0; // angle pos in world frame (in rads?)
+
+long prev_left_count = 0;
+long prev_right_count = 0;
 
 
 void setup() {
@@ -27,11 +28,39 @@ void setup() {
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){2,3,4,5}, SensorCount);
   
+  delay(200);
+ 
+  // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
+  // = ~25 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  
+  // print the calibration minimum values measured when emitters were on
   Serial.begin(9600);
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(200);
   
   // setup interrupts
   attachInterrupt(digitalPinToInterrupt(2),left_wheel_isr,CHANGE);
   attachInterrupt(digitalPinToInterrupt(3),right_wheel_isr,CHANGE);
+  delay(500);
 }
 
 // takes reading from qtr sensor, returns 1 if reading black, 0 if white
@@ -45,26 +74,29 @@ uint8_t black_or_white(int sensorValue) {
 
 // right wheel uses pins 3 and 4
 void right_wheel_isr() {
-    volatile static uint8_t right_enc_val = 0;
+    static int8_t lookup_table[] = {0,0,0,-1,0,0,1,0,0,1,0,0,-1,0,0,0};
+    static uint8_t enc_val = 0;
     
-    right_enc_val = right_enc_val << 2;
+    enc_val = enc_val << 2;
 
     uint8_t pin4 = black_or_white(sensorValues[2]);
     uint8_t pin3 = black_or_white(sensorValues[1]);
-    right_enc_val = right_enc_val | ((pin4 << 1) | pin3); // puts current values fo pins 5 and 2 into enc_val
-    right_count = right_count + lookup_table[right_enc_val & 0b1111];
+    enc_val = enc_val | ((pin4 << 1) | pin3); // puts current values fo pins 5 and 2 into enc_val
+    right_count = right_count + lookup_table[enc_val & 0b1111];
 }
 
 // left wheel uses pins 2 and 5
 void left_wheel_isr() {
-    volatile static uint8_t left_enc_val = 0;
+    static int8_t lookup_table[] = {0,0,0,-1,0,0,1,0,0,1,0,0,-1,0,0,0};
+    static uint8_t enc_val = 0;
     
-    left_enc_val = left_enc_val << 2;
+    enc_val = enc_val << 2;
 
     uint8_t pin5 = black_or_white(sensorValues[3]);
     uint8_t pin2 = black_or_white(sensorValues[0]);
-    left_enc_val = left_enc_val | ((pin5 << 1) | pin2); // puts current values fo pins 5 and 2 into enc_val
-    left_count = left_count + lookup_table[left_enc_val & 0b1111];
+   
+    enc_val = enc_val | ((pin5 << 1) | pin2); // puts current values fo pins 5 and 2 into enc_val
+    left_count = left_count + lookup_table[enc_val & 0b1111];
 }
 
 
@@ -81,7 +113,12 @@ long delta_x_robot(long deltaRight, long deltaLeft) {
 void loop() {
   prev_left_count = left_count;
   prev_right_count = right_count;
-  qtr.read(sensorValues);
+  qtr.readLineBlack(sensorValues);
+  for (uint8_t i = 0; i < SensorCount; i++) {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println();
 
 //  Serial.print(left_count);
 //  Serial.println();
