a0922f6590f63d49745802a6160ae9e806be3eb9
DJStefan <stefan.kussmaul@gmail.com>
2019-10-18 23:03:44 -0400

    Initial ArduinoToPiPacket, and some Arduino-side parsing code

--- arduino-serial-test/arduino-serial-test.ino
+++ arduino-serial-test/arduino-serial-test.ino
@@ -1,31 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+struct PiToArduinoPacket 
+{
+  uint8_t commandID;
+  float arg1;
+  float arg2;
+  float arg3;
+  uint8_t seqNum;
+};
+
+struct ArduinoToPiPacket 
+{
+  
+};
+
+void setup() 
+{
+  Serial.begin(9600);  // TODO: RUN AT A HIGHER BAUD RATE
+  Serial.flush();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-//  if (Serial.available()) {
-//    Serial.write(Serial.parseInt());
-//    Serial.flush();
-//  
-//  }
-//  for (byte n = 0; n < 255; n++) 
-//  {
-//    Serial.write(n);
-//    delay(50);
-//  }
-  Serial.println("Hello world");
-  if (Serial.available()) 
+void loop() 
+{
+  // Check if a packet has buffered.
+  while (Serial.available() >= 14) 
   {
-    Serial.print("There are bytes available: ");
-    while (Serial.available()) 
+    PiToArduinoPacket packet;
+    bool got_packet = readPacket(&packet);
+
+    if (got_packet) 
     {
-      Serial.print(Serial.read(), DEC);
+      
     }
-    Serial.println();
     
   }
-  delay(1000);
+}
+
+// TODO: ERROR CHECKING
+bool readPacket(PiToArduinoPacket* packet) 
+{
+//  uint8_t command_id = 
+  packet->commandID = Serial.read();
+  packet->arg1 = readUint32() * 1000.0;
+  packet->arg2 = readUint32() * 1000.0;
+  packet->arg3 = readUint32() * 1000.0;
+  packet->seqNum = Serial.read();
+}
+
+uint16_t readUint32()
+{
+  uint32_t u_int = 0;
+  uint8_t next_byte;
+
+  for (char i = 0; i < 4; i++) 
+  {
+    next_byte = Serial.read();
+    u_int = (u_int << 8) | next_byte;
+  }
 }
--- pi-serial-test.py
+++ pi-serial-test.py
@@ -1,12 +1,13 @@
 import serial
 
-# Command IDs
-CMD_1 = 1
-CMD_2 = 2
-CMD_3 = 3
-
+# TODO: PREAMBLES?
 class PiToArduinoPacket:
-    def __init__(self, commandID, arg1, arg2, arg3, seq_num):
+    # Command IDs
+    CMD_1 = 1
+    CMD_2 = 2
+    CMD_3 = 3
+
+    def __init__(self, commandID, seq_num, arg1, arg2=0, arg3=0):
         self.commandID = commandID
         self.arg1 = arg1
         self.arg2 = arg2
@@ -14,7 +15,7 @@ class PiToArduinoPacket:
         self.seq_num = seq_num
 
     # Convert to formatted byte string
-    def to_bytes(self):
+    def to_byte_string(self):
         args = []
         args.append(int(self.commandID).to_bytes(1, 'big'))
         args.append(int(1000 * self.arg1).to_bytes(4, 'big'))
@@ -23,11 +24,41 @@ class PiToArduinoPacket:
         args.append(int(self.seq_num).to_bytes(1, 'big'))
         return b''.join(args)
 
+
+class ArduinoToPiPacket:  # TODO: WHAT KIND OF ENDIANNESS?
+    # Command IDs
+    CMD_1 = 1
+    CMD_2 = 2
+    CMD_3 = 3
+
+    def parse_ufloat16(byte_array_4):
+        return int.from_bytes(bytes(byte_array_4), 'big') / 1000.0
+
+    def parse_uint16(byte_array_4):
+        return int.from_bytes(bytes(byte_array), 'big')
+
+    # Parse bytestring
+    def __init__(self, byte_array):
+        self.commandID = byte_array[0]
+        self.arg1 = ArduinoToPiPacket.parse_ufloat16(byte_array[1:5])
+        self.arg2 = ArduinoToPiPacket.parse_ufloat16(byte_array[5:9])
+        self.arg3 = ArduinoToPiPacket.parse_ufloat16(byte_array[9:13])
+        self.seq_num = byte_array[13]
+
+    def __str__(self):
+        return '{}: {} {} {} {}'.format(self.commandID, self.arg1, \
+            self.arg2, self.arg3, self.seq_num)
+
+
+
 # Sequence number
 seq_num = 0
 
-packet1 = PiToArduinoPacket(CMD_1, 1, 2, 3, seq_num)
-print (packet1.to_byte_array())
+packet1 = PiToArduinoPacket(PiToArduinoPacket.CMD_1, seq_num, 1, 2, 3)
+p1_str = packet1.to_byte_string()
+print(p1_str)
+packet2 = ArduinoToPiPacket(bytes(p1_str))
+print(packet2)
 
 # ser = serial.Serial('COM5', 9600)
 # ser.flushInput()
