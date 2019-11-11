7756cba1a2f6bf8d291010965e57724bf4ea4811
Arkani <stefan.kussmaul@gmail.com>
2019-11-11 13:52:46 -0500

    Multi-threaded camera interface+display working (display slow)

--- Python/image_pipeline.py
+++ Python/image_pipeline.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 # from PIL import Image
 import pygame
-# from picam_interface import Camera
+from picam_interface import Camera
 
 def process_img(m):
 	yellow = (254, 240, 82)
@@ -33,33 +33,41 @@ def process_img(m):
 				 abs(m[i][j][2] - white[2]) < white_tolerance:
 				m[i][j] = [0, 0, 255]
 
-IMAGE_RESOLUTION = (320, 240)  # TODO: IS THIS COLS * ROWS?
+IMAGE_RESOLUTION = (320, 240)  # TODO: MAKE RES_W, RES_H INSTEAD
 if __name__ == '__main__':
 	# Code to display numpy array using Pygame: https://stackoverflow.com/a/41171153
 	# Also, this may be helpful: https://stackoverflow.com/a/49593410
 	pygame.init()
-	display = pygame.display.set_mode(IMAGE_RESOLUTION)
+	display = pygame.display.set_mode((IMAGE_RESOLUTION[1], IMAGE_RESOLUTION[0]))
 
 	# TODO: HOW TO COPY 3-CHANNEL IMAGES INTO A SINGLE CHANNEL?
-	img = np.zeros(shape=(320, 480, 3), dtype='uint8')
-	img[:, :, 0].fill(255)
+	img = np.zeros(shape=(IMAGE_RESOLUTION[1], IMAGE_RESOLUTION[0], 3), dtype='uint8')
 	
 	running = True
+	
+	camera = Camera()
+	
 
 	# HOW TO DRAW THE IMAGE? THIS MAY BE HELPFUL: https://github.com/amremam2004/py2cairo/blob/2f1f3788fa2d9b693c77645edc44f35e7073aadd/test/pygame-test1.py
 	# Also: https://www.pygame.org/docs/ref/surfarray.html
 	# Also: https://www.pygame.org/docs/tut/SurfarrayIntro.html
 	while running:
+		print('Updating display')
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 					running = False
+		# Get next capture and copy to the img array
+		if camera.frame_ready:
+			print('Main sees that a frame is ready')
+			camera.get_frame(img)
+			process_img(img)
 		# Draw to the screen.
 		pixel_array = pygame.PixelArray(display)
 		# pixel_array[:, :] = (255, 255, 255)
 
 		# Copy the image into the surface (slow :(... Need to merge three channels into one)	
-		for i in range(IMAGE_RESOLUTION[0]):
-			for j in range(IMAGE_RESOLUTION[1]):
+		for i in range(IMAGE_RESOLUTION[1]):
+			for j in range(IMAGE_RESOLUTION[0]):
 				pixel_array[i, j] = (img[i, j, 0], img[i, j, 1], img[i, j, 2])
 		# print (pixel_array.shape)
 		# display.blit(pixel_array.surface, (0, 0))
@@ -67,20 +75,3 @@ if __name__ == '__main__':
 		pygame.display.update()
 
 	pygame.quit()
-
-	# with picamera.PiCamera() as camera:
-	# 	camera.resolution = (320, 240)
-	# 	camera.framerate = 24
-	# 	time.sleep(2)
-	# 	cam_output = np.empty((240, 320, 3), dtype=np.uint8)
-		
-	# 	camera.capture(cam_output, 'rgb')
-		
-		# WE CAN'T PREVIEW OVER TEAMVIEWER
-		#camera.start_preview()
-		#time.sleep(5)
-		#camera.stop_preview()
-
-	# process_img(cam_output)
-	# img = Image.fromarray(cam_output, 'RGB')
-	# img.save('output.png')
--- Python/picam_interface.py
+++ Python/picam_interface.py
@@ -7,8 +7,9 @@ Image capture concept: https://www.pyimagesearch.com/2015/03/30/accessing-the-ra
 '''
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-import threading import Thread
+from threading import Thread
 import time
+import numpy as np
 
 class Camera:
   def __init__(self, resolution=(320, 240), framerate=32):
@@ -16,17 +17,14 @@ class Camera:
     self.camera = PiCamera()
     self.camera.resolution = resolution
     self.camera.framerate = framerate
-    # IO stream where the raw pixel data will be stored.
-    # It is stored as a Numpy array with three channels (R, G, B)
-    self.raw_capture = PiRGBArray(camera, size=resolution)  # TODO: THIS CAN PROBABLY BE A LOCAL VARIABLE
+    self.frame_ready = False
+    self._last_frame = np.empty((resolution[1], resolution[0], 3), dtype='uint8')
     # Whether the camera is currently capturing images.
     # self.running = False
-    # The capture thread, which will run '_thread_capture_async()'
-    self.capture_thread = Thread(target=self._thread_capture_async, args=())
     # Sleep while the camera initializes
     time.sleep(0.1)
-
-    self.last_frame = None
+    # The capture thread, which will run '_thread_capture_async()'
+    self.capture_thread = Thread(target=self._thread_capture_async, args=())
 
     # Start!
     self.capture_thread.start()    
@@ -47,7 +45,8 @@ class Camera:
   # numpy array. The array must be the proper resolution!
   # TODO: USE MUTEX?
   def get_frame(self, buffer):
-    np.copy_to(buffer, self.last_frame)
+    np.copyto(buffer, self._last_frame)
+    self.frame_ready = False
 
   # The thread's function. Must have an infinite loop.
   def _thread_capture_async(self):
@@ -55,13 +54,18 @@ class Camera:
     # https://picamera.readthedocs.io/en/release-1.10/api_camera.html#picamera.camera.PiCamera.capture_continuous
     # https://picamera.readthedocs.io/en/release-1.10/api_array.html#pirgbarray
     # self.camera.start_preview()
-    
+    print('Starting capture thread')
+    # IO stream where the raw pixel data will be stored.
+    # It is stored as a Numpy array with three channels (R, G, B)
+    raw_capture = PiRGBArray(self.camera, size=self.camera.resolution)
     # Infinite capture loop.
-    for frame in self.camera.capture_continuous(self.raw_capture, format="rgb", use_video_port=True):
+    for frame in self.camera.capture_continuous(raw_capture, format="rgb", use_video_port=True):
+      print('Got next frame at ms {}'.format(time.time()))
       # Copy image data to 'last_frame'
-      np.copy_to(self.last_frame, frame.array)
+      np.copyto(self._last_frame, frame.array)
       # Clear the stream in preparation for the next frame.
-      output.truncate(0)
+      raw_capture.truncate(0)
+      self.frame_ready = True
       # TODO: FIRE FRAME_READY() LISTENER
 
     # camera_on = False
