a69119409c3f989c31d90b127a23249a7e33debc
Wilson <wilson.c132@gmail.com>
2019-11-21 17:06:41 -0500

    deleted some old commented out code

--- Python/navigator_test.py
+++ Python/navigator_test.py
@@ -1,11 +1,6 @@
 from navigator import *
 import time
 
-# G_ = DiGraph()
-# edges = ((1,2,{'attr': -1}), (2,3,{'attr': -2}))
-# G_.add_edges_from(edges)
-# print(shortest_path(G_, 1, 3))
-
 def test_route(route):
 	nav = Navigator(route)
 
@@ -16,9 +11,9 @@ def test_route(route):
 		instruct = nav.get_next_instruction()
 		print(instruct)
 
-start_time = time.time()
-for i in range(100):
-	test_route((6,9))
-print(time.time() - start_time)
 
-# test_route((1,12,6))
+# test_route(1,5,10)
+# start_time = time.time()
+# for i in range(100):
+# 	test_route((6,9))
+# print(time.time() - start_time)
